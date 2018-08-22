dc32c71eab81e6ec52814e89e3f1f953d48a72de
stacyelle <stacyelle@me.com>
2018-08-22 11:04:03 -0600

    lachlan saves the day

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,35 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const ensureAuthenticated = require('./auth').ensureAuthenticated;
+const ensureAuthenticated = require('../../auth.js').ensureAuthenticated;
 const User = require('../../models/user');
+
 
 router.all('*', ensureAuthenticated);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', {
-    user: User.findById({
-      where: {id: req.user.id}
-    }).then(user => {
-      console.log(user)
-      res.send(user)
-    }).catch(err => {
-      res.send(err)
-    })
+  User.findById({
+    where: {id: req.user.id}
+  }).then(user => {
+    console.log(user)
+    res.send(user)
+  }).catch(err => {
+    res.send(err)
   });
 });
 
-router.get('/user', function(req, res, next) {
-  res.render('users', {
-    user: User.findById({
-      where: {id: req.user.id}
-    }).then(user => {
-      console.log(user)
-      res.send(user)
-    }).catch(err => {
-      res.send(err)
-    })
-  });
-});
+
 
 module.exports = router;
